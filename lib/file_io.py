367663938d644363ada551bc95aac28af8bf8187
Elise Erickson <elisece717@gmail.com>
2024-02-09 19:03:40 -0500

    done:

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w") as my_file:
+        my_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a") as my_file:
+        my_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r") as my_file:
+        return(my_file.read())
